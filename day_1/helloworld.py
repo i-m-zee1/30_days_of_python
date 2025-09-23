b15c265107b6cfce0dd8fe36fbc3f3f88c02047f
Muhammad Zeeshan Ali <imzee.au@gmail.com>
2025-09-23 12:11:54 +0000

    Day 1 of 30 Days challange

---
+++ day_1/helloworld.py
@@ -0,0 +1,48 @@
+'''
+Exercise: Level 1
+'''
+import sys
+import platform
+
+print("Python version: ",sys.version) # Full version string with sys import
+print("Python version: ",sys.version_info)    # Tuple with major, minor, micro sys import
+
+print("Python version:", platform.python_version()) # Full version string with platform import
+print("Version tuple:", platform.python_version_tuple())  # Tuple with major, minor, micro with platform import
+
+
+print(2 + 3)             # addition(+)
+print(3 - 1)             # subtraction(-)
+print(2 * 3)             # multiplication(*)
+print(3 / 2)             # division(/)
+print(3 ** 2)            # exponential(**)
+print(3 % 2)             # modulus(%)
+print(3 // 2)            # Floor division operator(//)
+
+#Writing String
+print('Muhammad Zeeshan Ali')
+print("Mana")
+print('Pakistan')
+print('I am enjoying 30 days of python')
+
+#check data type of followings
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type('Muhammad Zeeshan Ali'))
+print(type('Mana'))
+print(type('Pakistan'))
+
+'''
+Exercise: Level 2
+'''
+
+'''
+Exercise: Level 3
+ - Write an example for different Python data types such as Number(Integer, Float, Complex), String, Boolean, List, Tuple, Set and Dictionary.
+ - Find an Euclidian distance between (2, 3) and (10, 8)
+
+'''
+
---
+++ helloworld.py
@@ -0,0 +1,34 @@
+
+print(2 + 3)             # addition(+)
+print(3 - 1)             # subtraction(-)
+print(2 * 3)             # multiplication(*)
+print(3 / 2)             # division(/)
+print(3 ** 2)            # exponential(**)
+print(3 % 2)             # modulus(%)
+print(3 // 2)            # Floor division operator(//)
+
+# Checking data types
+print(type(10))          # Int
+print(type(3.14))        # Float
+print(type(1 + 3j))      # Complex number
+print(type('Asabeneh'))  # String
+print(type([1, 2, 3]))   # List
+print(type({'name':'Asabeneh'})) # Dictionary
+print(type({9.8, 3.14, 2.7}))    # Set
+print(type((9.8, 3.14, 2.7)))    # Tuple
+
+#Writing String
+print('Muhammad Zeeshan Ali')
+print("Mana")
+print('Pakistan')
+print('I am enjoying 30 days of python')
+
+#check data type of followings
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type('Muhammad Zeeshan Ali'))
+print(type('Mana'))
+print(type('Pakistan'))
