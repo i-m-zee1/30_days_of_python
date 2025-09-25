e4af195eaee7736dc091c1e74b998634e69e7c2e
Muhammad Zeeshan Ali <imzee.au@gmail.com>
2025-09-25 12:32:07 +0000

    Day 4 of 30 Days Challange

--- day_4/strings.py
+++ day_4/strings.py
@@ -168,7 +168,7 @@ print(challenge.find('th')) # 0
 challenge = 'Thirty'
 print(challenge.isdigit()) # False
 challenge = '30'
-print(challenge.digit())   # True
+print(challenge.isdigit())   # True
 
 # isdecimal():Checks decimal characters
 
@@ -247,3 +247,169 @@ print(challenge.startswith('thirty')) # True
 challenge = '30 days of python'
 print(challenge.startswith('thirty')) # False
 
+'''
+💻 Exercises - Day 4
+Concatenate the string 'Thirty', 'Days', 'Of', 'Python' to a single string, 'Thirty Days Of Python'.
+Concatenate the string 'Coding', 'For' , 'All' to a single string, 'Coding For All'.
+Declare a variable named company and assign it to an initial value "Coding For All".
+Print the variable company using print().
+Print the length of the company string using len() method and print().
+Change all the characters to uppercase letters using upper() method.
+Change all the characters to lowercase letters using lower() method.
+Use capitalize(), title(), swapcase() methods to format the value of the string Coding For All.
+Cut(slice) out the first word of Coding For All string.
+Check if Coding For All string contains a word Coding using the method index, find or other methods.
+Replace the word coding in the string 'Coding For All' to Python.
+Change Python for Everyone to Python for All using the replace method or other methods.
+Split the string 'Coding For All' using space as the separator (split()) .
+"Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon" split the string at the comma.
+What is the character at index 0 in the string Coding For All.
+What is the last index of the string Coding For All.
+What character is at index 10 in "Coding For All" string.
+Create an acronym or an abbreviation for the name 'Python For Everyone'.
+Create an acronym or an abbreviation for the name 'Coding For All'.
+Use index to determine the position of the first occurrence of C in Coding For All.
+Use index to determine the position of the first occurrence of F in Coding For All.
+Use rfind to determine the position of the last occurrence of l in Coding For All People.
+Use index or find to find the position of the first occurrence of the word 'because' in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
+Use rindex to find the position of the last occurrence of the word because in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
+Slice out the phrase 'because because because' in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
+Find the position of the first occurrence of the word 'because' in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
+Slice out the phrase 'because because because' in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
+Does ''Coding For All' start with a substring Coding?
+Does 'Coding For All' end with a substring coding?
+'   Coding For All      '  , remove the left and right trailing spaces in the given string.
+Which one of the following variables return True when we use the method isidentifier():
+
+    30DaysOfPython
+    thirty_days_of_python
+
+The following list contains the names of some of python libraries: ['Django', 'Flask', 'Bottle', 'Pyramid', 'Falcon']. Join the list with a hash with space string.
+Use the new line escape sequence to separate the following sentences.
+
+I am enjoying this challenge.
+I just wonder what is next.
+
+Use a tab escape sequence to write the following lines.
+
+Name      Age     Country   City
+Asabeneh  250     Finland   Helsinki
+
+    Use the string formatting method to display the following:8
+
+radius = 10
+area = 3.14 * radius ** 2
+The area of a circle with radius 10 is 314 meters square.
+
+    Make the following using string formatting methods:
+
+8 + 6 = 14
+8 - 6 = 2
+8 * 6 = 48
+8 / 6 = 1.33
+8 % 6 = 2
+8 // 6 = 1
+8 ** 6 = 262144
+'''
+
+thirty='Thirty'
+days = 'Days'
+offff = 'Of'
+py = 'Python'
+string = thirty+' '+days+' '+offff+' '+py
+print(string)
+
+coding='Coding'
+forff = 'For'
+all = 'All'
+string = coding+' '+forff+' '+all
+print(string)
+
+company = 'Coding For All'
+print(company)
+
+print(len(company))
+
+print(company.upper())
+
+print(company.lower())
+
+print(company.capitalize())
+print(company.title())
+print(company.swapcase())
+
+print(company[1:])
+
+print(company.find('Coding'))
+print(company.find('Coding',1,9))
+print(company.rfind('Coding'))
+print(company.rfind('Coding',1,9))
+print(company.index('Coding'))
+# print(company.index('Coding',9))
+print(company.rindex('Coding'))
+# print(company.rindex('Coding',1,9))
+
+print(company.replace('Coding','Python'))
+
+pay = 'Python for Everyone'
+
+print(pay.replace('Everyone','All'))
+
+print(company.split(' '))
+
+print("Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon".split(','))
+
+print(company[0])
+
+print(company[-1])
+
+print(company[10])
+
+print(''.join([x[0].upper() for x in 'Python For Everyone'.split()]))
+print(''.join([x[0].upper() for x in 'Coding For All'.split()]))
+
+print(company.index('C'))
+print(company.index('F'))
+print('Coding For All People'.rfind('l'))
+
+string = 'You cannot end a sentence with because because because is a conjunction'
+
+print('You cannot end a sentence with because because because is a conjunction'.find('because'))
+print('You cannot end a sentence with because because because is a conjunction'.index('because'))
+
+print('You cannot end a sentence with because because because is a conjunction'.find('because',40))
+print('You cannot end a sentence with because because because is a conjunction'.rindex('because',40))
+
+print(string[string.find('because'):string.rindex('because',45)+7])
+
+print(string.find("because"))
+
+print(string[string.find('because'):string.rindex('because',45)+7])
+
+print('Coding for All'.startswith('Coding'))
+
+print('Coding for All'.endswith('coding'))
+
+print('   Coding For All      '.strip())
+
+print('30DaysOfPython'.isidentifier())
+print('thirty_days_of_python'.isidentifier())
+
+print('# '.join(['Django', 'Flask', 'Bottle', 'Pyramid', 'Falcon']))
+
+print('I am enjoying this challenge.\nI just wonder what is next.')
+print('Name\t\tAge\tCountry\tCity\nAsabeneh\t250\tFinland\tHelsinki')
+
+radius = 10
+area = 3.14 * radius ** 2
+print(f'The area of a circle with radius {radius} is {area} meters square.')
+
+a=8
+b=6
+print(f'{a} + {b} = {a+b}')
+print(f'{a} - {b} = {a-b}')
+print(f'{a} * {b} = {a*b}')
+print(f'{a} / {b} = {a/b:.2f}')
+print(f'{a} % {b} = {a%b}')
+print(f'{a} // {b} = {a//b}')
+print(f'{a} ** {b} = {a**b}')
