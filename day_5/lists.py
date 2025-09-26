ab0ee6faaf85ecb96e5d1059cab523baa2f7599a
Muhammad Zeeshan Ali <imzee.au@gmail.com>
2025-09-26 10:14:14 +0000

    Day 5  of 30 Days Challange, Initial Commit

---
+++ day_5/lists.py
@@ -0,0 +1,259 @@
+empty_list = list() # this is an empty list, no item in the list
+print(len(empty_list)) # 0
+
+fruits = ['banana', 'orange', 'mango', 'lemon']                     # list of fruits
+vegetables = ['Tomato', 'Potato', 'Cabbage','Onion', 'Carrot']      # list of vegetables
+animal_products = ['milk', 'meat', 'butter', 'yoghurt']             # list of animal products
+web_techs = ['HTML', 'CSS', 'JS', 'React','Redux', 'Node', 'MongDB'] # list of web technologies
+countries = ['Finland', 'Estonia', 'Denmark', 'Sweden', 'Norway']
+
+# Print the lists and it length
+print('Fruits:', fruits)
+print('Number of fruits:', len(fruits))
+print('Vegetables:', vegetables)
+print('Number of vegetables:', len(vegetables))
+print('Animal products:',animal_products)
+print('Number of animal products:', len(animal_products))
+print('Web technologies:', web_techs)
+print('Number of web technologies:', len(web_techs))
+print('Number of countries:', len(countries))
+
+# Modifying list
+
+fruits = ['banana', 'orange', 'mango', 'lemon'] 
+first_fruit = fruits[0] # we are accessing the first item using its index
+print(first_fruit)      # banana
+second_fruit = fruits[1]
+print(second_fruit)     # orange
+last_fruit = fruits[3]
+print(last_fruit) # lemon
+# Last index
+last_index = len(fruits) - 1
+last_fruit = fruits[last_index]
+
+# Accessing items
+fruits = ['banana', 'orange', 'mango', 'lemon'] 
+last_fruit = fruits[-1]
+second_last = fruits[-2]
+print(last_fruit)       # lemon
+print(second_last)      # mango
+
+# Slicing items
+fruits = ['banana', 'orange', 'mango', 'lemon'] 
+all_fruits = fruits[0:4] # it returns all the fruits
+# this is also give the same result as the above
+all_fruits = fruits[0:] # if we don't set where to stop it takes all the rest
+orange_and_mango = fruits[1:3] # it does not include the end index
+orange_mango_lemon = fruits[1:]
+
+fruits = ['banana', 'orange', 'mango', 'lemon'] 
+all_fruits = fruits[-4:] # it returns all the fruits
+# this is also give the same result as the above
+orange_and_mango = fruits[-3:-1] # it does not include the end index
+orange_mango_lemon = fruits[-3:]
+
+
+fruits = ['banana', 'orange', 'mango', 'lemon'] 
+fruits[0] = 'Avocado' 
+print(fruits)       #  ['avocado', 'orange', 'mango', 'lemon']
+fruits[1] = 'apple'
+print(fruits)       #  ['avocado', 'apple', 'mango', 'lemon']
+last_index = len(fruits) - 1
+fruits[last_index] = 'lime'
+print(fruits)        #  ['avocado', 'apple', 'mango', 'lime']
+
+# checking items
+fruits = ['banana', 'orange', 'mango', 'lemon']
+does_exist = 'banana' in fruits
+print(does_exist)  # True
+does_exist = 'lime' in fruits
+print(does_exist)  # False
+
+# Append
+fruits = ['banana', 'orange', 'mango', 'lemon']
+fruits.append('apple')
+print(fruits)           # ['banana', 'orange', 'mango', 'lemon', 'apple']
+fruits.append('lime')   # ['banana', 'orange', 'mango', 'lemon', 'apple', 'lime]
+print(fruits)
+
+# insert
+fruits = ['banana', 'orange', 'mango', 'lemon']
+fruits.insert(2, 'apple') # insert apple between orange and mango
+print(fruits)           # ['banana', 'orange', 'apple', 'mango', 'lemon']
+fruits.insert(3, 'lime')   # ['banana', 'orange', 'apple', 'mango', 'lime','lemon',]
+print(fruits)
+
+# remove
+fruits = ['banana', 'orange', 'mango', 'lemon']
+fruits.remove('banana')
+print(fruits)  # ['orange', 'mango', 'lemon']
+fruits.remove('lemon')
+print(fruits)  # ['orange', 'mango']
+
+# pop
+fruits = ['banana', 'orange', 'mango', 'lemon']
+fruits.pop()     
+print(fruits)       # ['banana', 'orange', 'mango']
+
+fruits.pop(0)     
+print(fruits)       # ['orange', 'mango'] 
+
+# del 
+fruits = ['banana', 'orange', 'mango', 'lemon']
+del fruits[0]     
+print(fruits)       # ['orange', 'mango', 'lemon']
+
+del fruits[1]     
+print(fruits)       # ['orange', 'lemon']
+del fruits
+print(fruits)       # This should give: NameError: name 'fruits' is not defined
+
+# clear
+fruits = ['banana', 'orange', 'mango', 'lemon']
+fruits.clear()     
+print(fruits)       # []
+
+# copying a lits
+
+fruits = ['banana', 'orange', 'mango', 'lemon']
+fruits_copy = fruits.copy()     
+print(fruits_copy)       # ['banana', 'orange', 'mango', 'lemon']
+
+# join
+positive_numbers = [1, 2, 3,4,5]
+zero = [0]
+negative_numbers = [-5,-4,-3,-2,-1]
+integers = negative_numbers + zero + positive_numbers
+print(integers)
+fruits = ['banana', 'orange', 'mango', 'lemon']
+vegetables = ['Tomato', 'Potato', 'Cabbage','Onion', 'Carrot'] 
+fruits_and_vegetables = fruits + vegetables
+print(fruits_and_vegetables )
+
+# join with extend
+num1 = [0, 1, 2, 3]
+num2= [4, 5,6]
+num1.extend(num2)
+print('Numbers:', num1)
+negative_numbers = [-5,-4,-3,-2,-1]
+positive_numbers = [1, 2, 3,4,5]
+zero = [0]
+
+negative_numbers.extend(zero)
+negative_numbers.extend(positive_numbers)
+print('Integers:', negative_numbers)
+fruits = ['banana', 'orange', 'mango', 'lemon']
+vegetables = ['Tomato', 'Potato', 'Cabbage','Onion', 'Carrot'] 
+fruits.extend(vegetables)
+print('Fruits and vegetables:', fruits )
+
+# count
+fruits = ['banana', 'orange', 'mango', 'lemon']
+print(fruits.count('orange'))   # 1
+ages = [22, 19, 24, 25, 26, 24, 25, 24]
+print(ages.count(24))           # 3
+
+# index
+fruits = ['banana', 'orange', 'mango', 'lemon']
+print(fruits.index('orange'))   # 1
+ages = [22, 19, 24, 25, 26, 24, 25, 24]
+print(ages.index(24)) 
+# Reverse
+fruits = ['banana', 'orange', 'mango', 'lemon']
+fruits.reverse()
+print(fruits)  
+ages = [22, 19, 24, 25, 26, 24, 25, 24]
+ages.reverse()
+print(ages) 
+
+# sort
+fruits = ['banana', 'orange', 'mango', 'lemon']
+fruits.sort()
+print(fruits) 
+fruits.sort(reverse=True)
+print(fruits)
+ages = [22, 19, 24, 25, 26, 24, 25, 24]
+ages.sort()
+print(ages) 
+ages.sort(reverse=True)
+print(ages) 
+
+'''
+💻 Exercises: Day 5
+Exercises: Level 1
+
+Declare an empty list
+
+Declare a list with more than 5 items
+
+Find the length of your list
+
+Get the first item, the middle item and the last item of the list
+
+Declare a list called mixed_data_types, put your(name, age, height, marital status, address)
+
+Declare a list variable named it_companies and assign initial values Facebook, Google, Microsoft, Apple, IBM, Oracle and Amazon.
+
+Print the list using print()
+
+Print the number of companies in the list
+
+Print the first, middle and last company
+
+Print the list after modifying one of the companies
+
+Add an IT company to it_companies
+
+Insert an IT company in the middle of the companies list
+
+Change one of the it_companies names to uppercase (IBM excluded!)
+
+Join the it_companies with a string '#;  '
+
+Check if a certain company exists in the it_companies list.
+
+Sort the list using sort() method
+
+Reverse the list in descending order using reverse() method
+
+Slice out the first 3 companies from the list
+
+Slice out the last 3 companies from the list
+
+Slice out the middle IT company or companies from the list
+
+Remove the first IT company from the list
+
+Remove the middle IT company or companies from the list
+
+Remove the last IT company from the list
+
+Remove all IT companies from the list
+
+Destroy the IT companies list
+
+Join the following lists:
+
+front_end = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+back_end = ['Node','Express', 'MongoDB']
+
+    After joining the lists in question 26. Copy the joined list and assign it to a variable full_stack, then insert Python and SQL after Redux.
+
+Exercises: Level 2
+
+    The following is a list of 10 students ages:
+
+ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+
+    Sort the list and find the min and max age
+    Add the min age and the max age again to the list
+    Find the median age (one middle item or two middle items divided by two)
+    Find the average age (sum of all items divided by their number )
+    Find the range of the ages (max minus min)
+    Compare the value of (min - average) and (max - average), use abs() method
+
+    Find the middle country(ies) in the countries list
+    Divide the countries list into two equal lists if it is even if not one more country for the first half.
+    ['China', 'Russia', 'USA', 'Finland', 'Sweden', 'Norway', 'Denmark']. Unpack the first three countries and the rest as scandic countries.
+
+'''
